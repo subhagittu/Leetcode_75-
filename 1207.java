c81ce6db939cd9382d6935180196c9a0660414a6
Subhadeep Das <121690779+subhagittu@users.noreply.github.com>
2023-11-06 23:43:29 +0530

    Create 1207.java

---
+++ 1207.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i : arr) {
+            map.put(i, map.getOrDefault(i, 0) + 1);
+        }
+
+        Set<Integer> set = map.entrySet().stream()
+        .map(Map.Entry::getValue)
+        .collect(Collectors.toSet());
+
+        return set.size() == map.size();
+    }
+}
