3179521e6b28223ce9173c745f4deb40c6dbdcd5
Subhadeep Das <121690779+subhagittu@users.noreply.github.com>
2023-11-11 23:58:04 +0530

    Create 735.cpp

---
+++ 735.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        vector<int> s;
+
+        for(auto& i : asteroids) {
+
+            if(!s.empty() && s.back() > 0 && i < 0) {
+                int flag = 1;
+                while(!s.empty() && s.back() > 0 && i < 0) {
+                    if(abs(s.back()) < abs(i)) {
+                        s.pop_back();
+                    }
+                    else if(abs(s.back()) == abs(i)) {
+                        s.pop_back();
+                        flag = 0;
+                        break;
+                    }
+                    else {
+                        flag = 0;
+                        break;
+                    }
+                }
+
+                if(flag) {
+                    s.push_back(i);
+                }
+
+            }
+            else {
+                s.push_back(i);
+            }
+        }
+
+        return s;
+    }
+};
