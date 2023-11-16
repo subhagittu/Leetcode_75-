da014e35729fe0fda0bf669546b5b9a39b18771f
Subhadeep Das <121690779+subhagittu@users.noreply.github.com>
2023-11-16 23:51:34 +0530

    Create 1161.cpp

---
+++ 1161.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxLevelSum(TreeNode* root) 
+    {
+        int maxsum=INT_MIN,levelctr=1,ans;
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty())
+        {
+            int ls=0,n=q.size();
+            for(int i=0;i<n;i++)
+            {
+                TreeNode* node=q.front();
+                q.pop();
+                if(node->left) q.push(node->left);
+                if(node->right) q.push(node->right);
+                ls+=node->val;
+            }
+            if(ls>maxsum)
+            {
+                maxsum=max(maxsum,ls);
+                ans=levelctr;
+            }
+            levelctr++;
+        }
+        return ans;
+    }
+};
