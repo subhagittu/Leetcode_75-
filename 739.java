abd34c36e914f43c7952c61fcc065a9da1e16f59
Subhadeep Das <121690779+subhagittu@users.noreply.github.com>
2023-12-08 23:50:53 +0530

    Create 739.java

---
+++ 739.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int[] answer = new int[temperatures.length];
+        Stack<Integer> indices = new Stack<>();  
+        for (int i = 0; i < temperatures.length; i++){
+            if (i+1 >= temperatures.length){
+                answer[i] = 0; 
+                while((!indices.isEmpty())){
+                    answer[indices.pop()] = 0; 
+                }
+            }
+            else{
+                if (temperatures[i] < temperatures[i+1]){
+                    while((!indices.isEmpty()) && (temperatures[indices.peek()] < temperatures[i+1])){
+                            answer[indices.peek()] = i+1 - indices.pop(); 
+                        }
+                        answer[i] = 1;
+                }
+                    
+                else{
+                    indices.push(i);
+                } 
+            }
+         
+        }
+        return answer;
+
+    }
+}
