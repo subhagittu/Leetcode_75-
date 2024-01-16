04c153a2db2c6ee681f929a0e004a0ea0141dc1e
Subhadeep Das <121690779+subhagittu@users.noreply.github.com>
2024-01-16 23:50:31 +0530

    Update 1207.java

--- 1207.java
+++ 1207.java
@@ -2,7 +2,7 @@ class Solution {
     public boolean uniqueOccurrences(int[] arr) {
         Map<Integer, Integer> map = new HashMap<>();
         for (int i : arr) {
-            map.put(i, map.getOrDefault(i, 0) + 1);
+            map.put(i, map.getOrDefault(i, 0) + 1); 
         }
 
         Set<Integer> set = map.entrySet().stream()
