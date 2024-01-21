18c5817631ccbe8fca9c48367f77f7270166a5fb
Subhadeep Das <121690779+subhagittu@users.noreply.github.com>
2024-01-21 23:40:56 +0530

    Update 1318.cpp

--- 1318.cpp
+++ 1318.cpp
@@ -7,7 +7,7 @@ public:
      for(int i = 0; (1 << i) <= max(max(a, b), c); i++)
      {
             int bit = 1 << i;
-            int a_bit = a & bit ? 1 : 0;
+            int a_bit = a & bit ? 1 : 0; 
             int b_bit = b & bit ? 1 : 0;
             int c_bit = c & bit ? 1 : 0;
             if(!(a_bit | b_bit) && c_bit)
