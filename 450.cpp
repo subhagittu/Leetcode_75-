b94fed46ea98291d00a65a967619e14d76efb6a0
Subhadeep Das <121690779+subhagittu@users.noreply.github.com>
2024-01-22 23:53:06 +0530

    Update 450.cpp

--- 450.cpp
+++ 450.cpp
@@ -3,6 +3,7 @@ public:
     TreeNode* deleteNode(TreeNode* root, int key) {
         if(root) 
             if(key < root->val) root->left = deleteNode(root->left, key);    
+                
             else if(key > root->val) root->right = deleteNode(root->right, key);       
             else{
                 if(!root->left && !root->right) return NULL;          
