54898670d0ba1776d85449f015a58411ac5db1bd
Subhadeep Das <121690779+subhagittu@users.noreply.github.com>
2024-01-23 23:43:44 +0530

    Update 872.java

--- 872.java
+++ 872.java
@@ -1,20 +1,27 @@
 class Solution {
     int index = 0;
-    public boolean verifyTree(TreeNode root,List<Integer> l){
+    public boolean verifyTree(TreeNode root,List<Integer> l)
+    
+    {
         if(root==null)return true;
         boolean left = verifyTree(root.left,l);
         boolean right = verifyTree(root.right,l);
        
-       if(root.left==null && root.right==null){
+       if(root.left==null && root.right==null)
+       {
            if(index>l.size())return false;
-            if(index<l.size() && root.val!=l.get(index)){
+            if(index<l.size() && root.val!=l.get(index))
+            {
                 return false;
             }
             index++;
         }
         return left && right;
     }
-    public void findLeafNodes(TreeNode root,List<Integer> l){
+
+    
+    public void findLeafNodes(TreeNode root,List<Integer> l)
+    {
         if(root==null)return;
         if(root.left==null && root.right==null){
             l.add(root.val);
@@ -22,6 +29,8 @@ class Solution {
         findLeafNodes(root.left,l);
         findLeafNodes(root.right,l);
     }
+
+    
     public boolean leafSimilar(TreeNode root1, TreeNode root2) {
         List<Integer> list = new ArrayList<>();
         findLeafNodes(root1,list);
