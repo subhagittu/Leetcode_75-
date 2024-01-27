d19a911f301eaae9996d22692c4cdef46fc548e0
Subhadeep Das <121690779+subhagittu@users.noreply.github.com>
2024-01-27 23:53:38 +0530

    Update 1143.java

--- 1143.java
+++ 1143.java
@@ -3,7 +3,7 @@ class Solution {
         int m=s1.length(); 
         int n=s2.length();
         int dp[][]=new int[m+1][n+1];
-        // dp[0][0]=1;
+        // dp[0][0]=1; 
         for(int i=1;i<m+1;i++){
             for(int j=1;j<n+1;j++){
                 if(s1.charAt(i-1)==s2.charAt(j-1)){
