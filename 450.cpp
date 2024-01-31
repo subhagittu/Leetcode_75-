40d6b4af514845bfb10eee5bca6e28b029a96e82
Subhadeep Das <121690779+subhagittu@users.noreply.github.com>
2024-01-31 23:51:03 +0530

    Update 450.cpp

--- 450.cpp
+++ 450.cpp
@@ -1,11 +1,13 @@
 class Solution {
 public:
-    TreeNode* deleteNode(TreeNode* root, int key) {
+    TreeNode* deleteNode(TreeNode* root, int key) 
+{
         if(root) 
             if(key < root->val) root->left = deleteNode(root->left, key);    
                 
             else if(key > root->val) root->right = deleteNode(root->right, key);       
-            else{
+            else
+            {
                 if(!root->left && !root->right) return NULL;          
                 if (!root->left || !root->right)
                     return root->left ? root->left : root->right;    
