6ea197563ff2612b36f232c49121af3a837bedd9
Subhadeep Das <121690779+subhagittu@users.noreply.github.com>
2024-02-22 23:47:09 +0530

    Update 790.cpp

--- 790.cpp
+++ 790.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int numTilings(int n) {
+    int numTilings(int n) { 
         int first  = 0 ,second = 1 , third = 1 , next , mod = 1e9+7 ;
         while(--n)
         {
