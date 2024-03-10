3759933a6b70c284c12554c6c38743dce90f0e18
Subhadeep Das <121690779+subhagittu@users.noreply.github.com>
2024-03-10 23:16:37 +0530

    Update 1143.java

--- 1143.java
+++ 1143.java
@@ -4,7 +4,7 @@ class Solution {
         int n=s2.length(); 
         int dp[][]=new int[m+1][n+1];
         // dp[0][0]=1;   
-        for(int i=1;i<m+1;i++){
+        for(int i=1;i<m+1;i++){  
             for(int j=1;j<n+1;j++){
                 if(s1.charAt(i-1)==s2.charAt(j-1)){
                     dp[i][j]=dp[i-1][j-1]+1;
